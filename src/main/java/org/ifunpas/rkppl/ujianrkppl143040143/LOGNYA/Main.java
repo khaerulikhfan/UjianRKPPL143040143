caf8f484d5b3259efaf64cc9a9c4661d7bc0a65d
khaerul <khaerul.ikhfan@gmail.com>
2016-05-13 14:16:25 +0700

    commit pertama, baru sampai setting log propertiesnya

---
+++ src/main/java/org/ifunpas/rkppl/ujianrkppl143040143/LOGNYA/Main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ifunpas.rkppl.ujianrkppl143040143.LOGNYA;
+
+/**
+ *
+ * @author SB 604-14
+ */
+public class Main {
+    
+}
---
+++ src/main/java/org/ifunpas/rkppl/ujianrkppl143040143/SERVICE/LogService.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ifunpas.rkppl.ujianrkppl143040143.SERVICE;
+
+/**
+ *
+ * @author SB 604-14
+ */
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+public class LogService {
+    private static final Logger logger = Logger.getLogger(LogService.class);
+}
